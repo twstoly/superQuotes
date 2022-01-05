c59ad8a3400c42c85e2436aaae53d605264cd0bd
Oliver <oliver.mwangale@moringaschool.com>
2022-01-05 07:24:34 +0300

    update the quote component - it containes the quote details that are added by the user

--- src/app/Components/quote/quote.component.ts
+++ src/app/Components/quote/quote.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Data } from 'src/app/Class/data';
 
 @Component({
   selector: 'app-quote',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  @Input() quote!:Data
   constructor() { }
 
   ngOnInit(): void {
