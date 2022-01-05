317ffc884cceae76526bd284b5297a98b7dfad44
Oliver <oliver.mwangale@moringaschool.com>
2022-01-05 07:35:28 +0300

    pass the main-control component to the app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountPipe } from './Pipes/count.pipe';
+import { MainControlComponent } from './Components/main-control/main-control.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { QuoteComponent } from './Components/quote/quote.component';
+import { QuoteDetailsComponent } from './Components/quote-details/quote-details.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { FormComponent } from './Components/form/form.component';
+import { BackgroundDirective } from './Directives/background.directive';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountPipe,
+    NavComponent,
+    MainControlComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    FooterComponent,
+    FormComponent,
+    BackgroundDirective
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
